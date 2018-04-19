d5a49b24f44c42a14198cc36d5340f551f1fc193
Boye Borg <boye.borg@icloud.com>
2018-04-19 21:41:17 +0200

    add lifecycle exceptions

---
+++ src/main/java/com/github/boyeborg/spritz/exceptions/NotYetProcessedException.java
@@ -0,0 +1,11 @@
+package com.github.boyeborg.spritz.exceptions;
+
+public class NotYetProcessedException extends Exception {
+
+	private static final long serialVersionUID = -7260240887240257538L;
+
+	public NotYetProcessedException(String message) {
+		super(message);
+	}
+
+}
